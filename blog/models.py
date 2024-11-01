9550a88b049f5234fdd48d623637a5ac114e03ea
alejoalbornoz <alejoalbornoz912@gmail.com>
2024-11-01 16:44:32 -0300

    Added blog app

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User #Esta clase User tiene todos los usarios registrados en nuestro panel de administrador
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published = models.DateTimeField(default=now)
+    image = models.ImageField(upload_to="blog", null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.title
--- webempresa/urls.py
+++ webempresa/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
-from core import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     #---
     path("", include("core.urls")),
+    path("services/", include("services.urls")),
 ]
     
 if settings.DEBUG:
