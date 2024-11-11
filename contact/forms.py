ba033420c9066f7a60cb7cf5a0bca3ac0170aec3
alejoalbornoz <alejoalbornoz912@gmail.com>
2024-11-11 10:03:36 -0300

    Updated Forms

--- contact/forms.py
+++ contact/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class':'form-control',"placeholder":"Nombre"}))                           
-    email = forms.EmailField(label="Email", required=True, widget=forms.TextInput(attrs={'class':'form-control',"placeholder":"ejemplo@gmail.com"}))
-    content = forms.CharField(label="Mensaje", required=True, widget=forms.Textarea(attrs={'class':'form-control',"placeholder":"Escribe tu mensaje"}))
+    name = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class':'form-control',"placeholder":"Nombre"}), min_length=3, max_length=100)                           
+    email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput(attrs={'class':'form-control',"placeholder":"ejemplo@gmail.com"}), min_length=3, max_length=100)
+    content = forms.CharField(label="Mensaje", required=True, widget=forms.Textarea(attrs={'class':'form-control',"rows": 3 ,"placeholder":"Escribe tu mensaje"}), min_length=3, max_length=100)
 
 
